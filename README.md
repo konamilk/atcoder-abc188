45fb534706e71fcfa0a486dca3ead5d83d97b44c
pinana <hina.no.atelier@gmail.com>
2021-01-25 15:17:17 +0900

    solve d with an editorial

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # atcoder-abc188
+
+|  problem  |  status |
+| ---- | ---- |
+|  A  |  AC  |
+|  B  |  AC  |
+|  C  |  AC  |
+|  D  |  AC(解説)  |
+|  E  |   - |
+|  F  |  -  |
--- src/bin/d.rs
+++ src/bin/d.rs
@@ -1,24 +1,29 @@
 use proconio::input;
-// use std::cmp;
+use std::cmp;
 
 fn main() {
     input! {
-        (n, prime): (usize, i64)
+        (n, c_max): (usize, i64),
+        abc : [(usize, usize, i64); n]
     }
+    let mut events = vec![];
+    for i in 0..n{
+        events.push((abc[i].0 - 1, abc[i].2));
+        events.push((abc[i].1, - abc[i].2));
+    }
+    events.sort();
 
-    let mut a: Vec<usize> = Vec::new();
-    let mut b: Vec<usize> = Vec::new();
-    let mut c: Vec<i64> = Vec::new();
+    let mut ans = 0;
+    let mut fee = 0i64;
+    let mut x_prev = 0;
 
-    for _i in 0..n{
-        input! {
-            (a_tmp, b_tmp, c_tmp): (usize, usize, i64)
+    for (x, y) in events {
+        if x != x_prev{
+            ans += cmp::min(fee, c_max) * (x - x_prev) as i64;
+            x_prev = x;
         }
-        a.push(a_tmp);
-        b.push(b_tmp);
-        c.push(c_tmp);
+        fee += y;
     }
 
-    println!("{:?} {:?}", n, prime);
-    println!("{:?} {:?} {:?}", a, b, c);
+    println!("{}", ans);
 }
